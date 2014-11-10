31808612bcd1997b382f8c0b837fb2990d705d14
Rafael Santos <formigarafa@gmail.com>
2014-11-11 05:13:26 +1300

    creating posts via api

--- stream_server/app/controllers/api/posts_controller.rb
+++ stream_server/app/controllers/api/posts_controller.rb
@@ -1,4 +1,18 @@
 module Api
   class PostsController < ApplicationController
+
+    respond_to :json
+    def index
+      respond_with current_user.posts
+    end
+
+    def create
+      post = current_user.posts.create post_params
+      render json: PostSerializer.new(post)
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body, :kind)
+    end
   end
 end
