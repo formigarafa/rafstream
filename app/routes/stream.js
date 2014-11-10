1bc75cadf8ff2c50c8b9a28ce32a1be91455425f
Rafael Santos <formigarafa@gmail.com>
2014-11-11 06:54:26 +1300

    send user to login if not logged in

--- app/routes/stream.js
+++ app/routes/stream.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    var appModel = this.modelFor('application');
+    var signedIn = !! appModel && appModel.get('currentUser.signedIn');
+    if(!signedIn) {
+      this.transitionTo('sign_in');
+    }
+  },
   model: function() {
     return this.store.find('event');
   }
